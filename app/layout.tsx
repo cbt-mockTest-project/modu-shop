fb87542fa9becc62929d7a7e03f9fde99e357ab3
eungwang <scm323@naver.com>
2024-09-18 15:21:33 +0900

    ga 설치

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import localFont from "next/font/local";
 import StyledComponentsRegistry from "./_providers/StyledComponentsRegistry";
 import BasicLayout from "./_components/BasicLayout";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Suspense } from "react";
 
 const pretendard = localFont({
@@ -33,6 +34,7 @@ export default function RootLayout({
           </StyledComponentsRegistry>
         </AntdRegistry>
       </body>
+      <GoogleAnalytics gaId="G-Q2RW54HJ4Q" />
     </html>
   );
 }
